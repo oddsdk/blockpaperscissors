c12cbf0c62fcc76f3450c1c182d73d7838d75b46
avivash <av@andrewvivash.com>
2023-03-10 22:45:44 -0800

    Feat: add persona selector

--- src/routes/style/+page.svelte
+++ src/routes/style/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
+
   import Divider from '$components/common/Divider.svelte'
+
+  let personaSelected = false
+  let persona: string
+
+  const handlePersonaClick = (p: string): void => {
+    personaSelected = true
+    persona = p
+  }
+
+  const handleButtonClick = (): void => {
+    goto(`/${persona}`)
+  }
 </script>
 
 <Divider align="right" size="small" />
@@ -14,9 +28,9 @@
 
 <div class="flex flex-col gap-8 py-8">
   <label class="flex flex-col gap-4" for="builder">
-    <h3 class="flex items-center space-x-4 text-xl uppercase">
-      <input name="style" id="builder" value="builder" type="radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/> 
-      <img class="w-[27px] h-auto" src={`${window.location.origin}/builder.png`} alt="Builder" /> 
+    <h3 class="flex items-center gap-3 text-xl uppercase">
+      <button on:click={() => handlePersonaClick('builder')} class="w-6 h-6 rounded-full border-black-500 border-[5px] transition-colors ease-in-out {persona === 'builder' ? 'bg-black-500' : ''}"></button>
+      <img class="w-[27px] h-auto" src={`${window.location.origin}/builder.png`} alt="Builder" />
       <span>Builder</span>
     </h3>
     <p class="text-base">
@@ -25,9 +39,9 @@
   </label>
 
   <label class="flex flex-col gap-4" for="speculator">
-    <h3 class="flex items-center space-x-4 text-xl uppercase">
-      <input name="style" id="speculator" value="speculator" type="radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/> 
-      <img class="w-[27px] h-auto" src={`${window.location.origin}/speculator.png`} alt="Speculator" /> 
+    <h3 class="flex items-center gap-3 text-xl uppercase">
+      <button on:click={() => handlePersonaClick('speculator')} class="w-6 h-6 rounded-full border-black-500 border-[5px] transition-colors ease-in-out {persona === 'speculator' ? 'bg-black-500' : ''}"></button>
+      <img class="w-[27px] h-auto" src={`${window.location.origin}/speculator.png`} alt="Speculator" />
       <span>Speculator</span>
     </h3>
     <p class="text-base">
@@ -36,8 +50,8 @@
   </label>
 
   <label class="flex flex-col gap-4" for="artist">
-    <h3 class="flex items-center space-x-4 text-xl uppercase">
-      <input name="style" id="artist" value="artist" type="radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/> 
+    <h3 class="flex items-center gap-3 text-xl uppercase">
+      <button on:click={() => handlePersonaClick('artist')} class="w-6 h-6 rounded-full border-black-500 border-[5px] transition-colors ease-in-out {persona === 'artist' ? 'bg-black-500' : ''}"></button>
       <img class="w-[27px] h-auto" src={`${window.location.origin}/artist.png`} alt="Artist" />
       <span>Artist</span>
     </h3>
@@ -47,6 +61,6 @@
   </label>
 </div>
 
-<a class="btn btn-primary btn-lg w-full mb-4 text-lg uppercase rounded-none" href="/style">
+<button disabled={!personaSelected} on:click={handleButtonClick} class="btn btn-primary btn-lg w-full mb-4 text-lg uppercase rounded-none">
   Get at it
-</a>
+</button>
