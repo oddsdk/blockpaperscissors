e4dbcd4ee384d1181b43b6e2e80561e7ea6b710d
avivash <av@andrewvivash.com>
2023-03-11 13:15:43 -0800

    Feat: fetch startingBlock before events start coming through

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -8,8 +8,26 @@ export type Network = {
 }
 
 export const CONTRACT_ADDRESS = '0xd9145CCE52D386f254917e481eB44e9943F39138'
+// const RPC_URL = 'https://api.hyperspace.node.glif.io/rpc/v1'
 const WS_PROVIDER = 'wss://wss.hyperspace.node.glif.io/apigw/lotus/rpc/v0'
 const wsProvider = new ethers.WebSocketProvider(WS_PROVIDER)
+// const providerRPC = {
+//   // TODO: uncomment this once we have a mainnet RPC URL
+//   // mainnet: {
+//   //   name: 'mainnet',
+//   //   rpc: RPC_URL,
+//   //   chainId: 314
+//   // },
+//   hyperspace: {
+//     name: 'hyperspace',
+//     rpc: RPC_URL,
+//     chainId: 3141,
+//   }
+// }
+// const provider = new ethers.JsonRpcProvider(providerRPC.hyperspace.rpc, {
+//   chainId: providerRPC.hyperspace.chainId,
+//   name: providerRPC.hyperspace.name
+// })
 
 /**
  * Initialise the networkStore and have it listen for blockNumber change
@@ -18,6 +36,13 @@ export const initialise = async (): Promise<void> => {
   const network = getStore(networkStore)
 
   if (!network.blockNumber) {
+    // `block` events take a little while to start coming through, so we'll fetch the startingBlock first
+    const startingBlock = await wsProvider.getBlockNumber()
+    networkStore.update(state => ({
+      ...state,
+      blockNumber: startingBlock,
+    }))
+
     wsProvider.on('block', blockNumber => {
       networkStore.update(state => ({
         ...state,
