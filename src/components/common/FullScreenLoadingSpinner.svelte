9185421a8766aa04bdec708443fc5696aa383407
avivash <av@andrewvivash.com>
2023-03-14 14:15:07 -0700

    Feat: replace loading spinner with icons

--- src/components/common/FullScreenLoadingSpinner.svelte
+++ src/components/common/FullScreenLoadingSpinner.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import LoadingSpinner from '$components/common/LoadingSpinner.svelte'
+  import IconLoader from '$components/common/IconLoader.svelte'
+
+  export let style = 'icons'
 </script>
 
 <div class="w-full h-[calc(100vh-72px)] flex items-center justify-center">
-  <LoadingSpinner />
+  {#if style === 'icons'}
+    <IconLoader />
+  {:else}
+    <LoadingSpinner />
+  {/if}
 </div>
---
+++ src/components/common/IconLoader.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import BlockMediumIcon from '$components/icons/BlockMedium.svelte'
+  import PaperMediumIcon from '$components/icons/PaperMedium.svelte'
+  import ScissorsMediumIcon from '$components/icons/ScissorsMedium.svelte'
+</script>
+
+<div class="relative my-auto">
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-fadeinout"><BlockMediumIcon /></div>
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-fadeinout animation-delay-[150ms] opacity-0"><PaperMediumIcon /></div>
+  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-fadeinout animation-delay-[300ms] opacity-0"><ScissorsMediumIcon /></div>
+</div>
