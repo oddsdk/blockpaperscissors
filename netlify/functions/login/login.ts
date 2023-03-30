6f75be6baee2cc9c80bd7c0af37ce78edfadd7e0
avivash <av@andrewvivash.com>
2023-03-30 10:48:47 -0700

    test privy client

--- netlify/functions/login/login.ts
+++ netlify/functions/login/login.ts
@@ -1,7 +1,16 @@
 import { Handler } from '@netlify/functions'
+import { PrivyClient } from '@privy-io/privy-node'
 
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters
+
+
+  const client = new PrivyClient(
+    'clfjzbafx000hmp08r3l8mj7c',
+    'ULxSa2Z7E45Q23Zbfp9LZh4JX8JeufkWNk3e3Rw5QYwDtGZL9eZymTNFY9JHzT5xzeZkoormARSGb5PoLvHpkE4'
+  )
+
+  console.log('client', client)
 
   return {
     statusCode: 200,
