c660bb6885fea36e92a38def064f3c6fd71a299e
avivash <av@andrewvivash.com>
2023-05-18 13:25:25 -0700

    Feat: votes so far

--- src/components/play/InfoCarousel.svelte
+++ src/components/play/InfoCarousel.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { page } from '$app/stores'
 
-  import { contractStore } from '$src/stores'
+  import { contractStore, networkStore } from '$src/stores'
   import { COLOR_MAP, WINNING_MOVES_MAP } from '$lib/contract'
 
   $: previousMove = $contractStore?.previousWinner?.result
@@ -9,6 +10,7 @@
   $: highVotes = 0
   $: lowVotes = 0
 
+  // Get the highest and lowest number of votes out of the last 10 blocks
   const getHighLowVotes = () => {
     Array.prototype.max = function() {
       return Math.max.apply(null, this);
@@ -22,7 +24,6 @@
     highVotes = votesPerBlock.max()
     lowVotes = votesPerBlock.min()
   }
-
   getHighLowVotes()
 
   // Daisy UI doesn't have a way of detecting the active slide of a carousel, so we have to add some extra logic
@@ -34,6 +35,7 @@
   let activeSlide = dots.find(dot => dot === $page.url.hash) || dots[0]
   let scrollContainer
   let slideWidth
+  // Track the scroll positon of the carousel and update the activeSlide state accordingly
   const trackCarouselScroll = () => {
     const scrollPosition = scrollContainer.scrollLeft
 
@@ -45,7 +47,7 @@
       activeSlide = dots[2]
     }
   }
-
+  // Scroll to a given slide
   const scrollToSlide = (slide) => {
     let scrollTarget = 0
     if (slide === dots[1]) {
@@ -55,6 +57,50 @@
     }
     scrollContainer.scroll({ left: scrollTarget, behavior: 'smooth' });
   }
+
+  // Parse votes cast from the mempool for the next block
+  let votesSoFar = 0
+  let blockVotes = 0
+  let paperVotes = 0
+  let scissorsVotes = 0
+  let blockPercentage = 0
+  let paperPercentage = 0
+  let scissorsPercentage = 0
+  const parseVotesForNextBlock = () => {
+    let nextBlockHeight = Number($networkStore.blockHeight)
+    let pendingTransactions = $networkStore.pendingTransactions
+
+    if (pendingTransactions.length > 0) {
+      let pendingTransactionsForNextBlock = pendingTransactions.filter((tx) => tx?.blockHeight === nextBlockHeight)
+      if (pendingTransactionsForNextBlock.length > 0) {
+        votesSoFar = pendingTransactionsForNextBlock.length
+        blockVotes = pendingTransactions.filter(tx => tx.choice === 'block').length
+        paperVotes = pendingTransactions.filter(tx => tx.choice === 'paper').length
+        scissorsVotes = pendingTransactions.filter(tx => tx.choice === 'scissors').length
+        const totalVotes = blockVotes + paperVotes + scissorsVotes
+        blockPercentage = (blockVotes / totalVotes) * 100
+        paperPercentage = (paperVotes / totalVotes) * 100
+        scissorsPercentage = (scissorsVotes / totalVotes) * 100
+      }
+    } else {
+      votesSoFar = 0
+      blockVotes = 0
+      paperVotes = 0
+      scissorsVotes = 0
+      blockPercentage = 0
+      paperPercentage = 0
+      scissorsPercentage = 0
+    }
+  }
+  parseVotesForNextBlock()
+
+  onMount(() => {
+    const unsubscribe = networkStore.subscribe(() => {
+      parseVotesForNextBlock()
+    })
+
+    return () => unsubscribe()
+  })
 
   $: {
     getHighLowVotes()
@@ -67,9 +113,35 @@
       <p class="uppercase font-bold text-[12px]">To keep the streak</p>
       <p class="text-lg text-center"><span class="uppercase font-bold {COLOR_MAP[WINNING_MOVES_MAP[previousMove]]?.text}">{WINNING_MOVES_MAP[previousMove]}</span> beats <span class="uppercase font-bold {COLOR_MAP[previousMove]?.text}">{previousMove}</span></p>
     </div>
-    <div id="slide2" class="carousel-item w-full h-full flex-col gap-4 pt-5 pb-9 items-center justify-center">
-      <p class="uppercase font-bold text-[12px]">120 votes so far</p>
-      <p class="text-lg text-center">LOW: 120 / HIGH: 320</p>
+    <div id="slide2" class="carousel-item w-full h-full flex-col pt-5 {votesSoFar ? 'gap-1 pb-[10px]' : 'gap-4 pb-9'} items-center justify-center">
+      <p class="uppercase font-bold text-[12px]">{votesSoFar} votes so far</p>
+
+      {#if blockVotes === 0 && paperVotes === 0 && scissorsVotes === 0}
+        <p class="text-lg text-center">No votes cast for next block</p>
+      {:else}
+        <div class="w-full pt-[22px] px-6 h-[52px] overflow-x-hidden">
+          <div class="relative w-full h-2 rounded-l-xl rounded-r-xl">
+            <div class="absolute z-10 top-0 left-0 bottom-0 transition-all w-[{blockPercentage}%] {COLOR_MAP.block.bg}">
+              <div class="absolute left-0 bottom-[calc(100%+4px)] flex items-center overflow-visible w-[110px]">
+                <img class="w-4 h-auto" src={`${window.location.origin}/block.svg`} alt="block" />
+                <p class="uppercase text-[10px] ml-1.5">{blockVotes}</p>
+              </div>
+            </div>
+            <div class="absolute z-10 top-0 {blockPercentage > 60 ? `right-[${scissorsPercentage}%]` : `left-[${blockPercentage}%]`} bottom-0 transition-all w-[{paperPercentage}%] {COLOR_MAP.paper.bg}">
+              <div class="absolute left-0 top-[calc(100%+4px)] flex items-center overflow-visible w-[110px]">
+                <img class="w-4 h-auto" src={`${window.location.origin}/paper.svg`} alt="paper" />
+                <p class="uppercase text-[10px] ml-1.5">{paperVotes}</p>
+              </div>
+            </div>
+            <div class="absolute z-10 top-0 right-0 bottom-0 transition-all w-[{scissorsPercentage}%] {COLOR_MAP.scissors.bg}">
+              <div class="absolute right-0 bottom-[calc(100%+4px)] flex items-center justify-end overflow-visible w-[110px]">
+                <p class="uppercase text-[10px] mr-1.5">{scissorsVotes}</p>
+                <img class="w-4 h-auto" src={`${window.location.origin}/scissors.svg`} alt="scissors" />
+              </div>
+            </div>
+          </div>
+        </div>
+      {/if}
     </div>
     <div id="slide3" class="carousel-item w-full h-full flex-col gap-4 pt-5 pb-9 items-center justify-center">
       <p class="uppercase font-bold text-[12px]"># OF VOTES (PAST 10 MOVES)</p>
