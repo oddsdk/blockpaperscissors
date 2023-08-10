be62da23aa248b5ca5223f126b26a286e606083b
avivash <av@andrewvivash.com>
2023-08-10 15:47:14 -0700

    Feat: add fund route with fund ring widget

---
+++ src/routes/fund/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <script src={`${window.location.origin}/fund-ring-widget.js`}></script>
+</svelte:head>
+
+<fund-ring-widget
+  contractAddress="0xA369Ae5472c0F6E78BaBb73Baa624a5F694DEe51"
+  title="Help fund the SLW RK'D!"
+  bodyCopy="We're trying to build the world's slowest arcade. With your help, we can realize this vision a little less slowly"
+/>
