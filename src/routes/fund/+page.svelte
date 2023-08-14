47ed84267d602652e62fa61d22e8e099988894f2
avivash <av@andrewvivash.com>
2023-08-14 13:19:42 -0700

    Fix: embedded widget and add branding to fund route

--- src/routes/fund/+page.svelte
+++ src/routes/fund/+page.svelte
@@ -1,6 +1,25 @@
+<script lang="ts">
+  import Divider from '$components/common/Divider.svelte'
+  import BlockIcon from '$components/icons/Block.svelte'
+  import PaperIcon from '$components/icons/Paper.svelte'
+  import ScissorsIcon from '$components/icons/Scissors.svelte'
+</script>
+
 <svelte:head>
   <script src={`${window.location.origin}/fund-ring-widget.js`}></script>
 </svelte:head>
+
+<Divider align="right" size="small" />
+
+<h1 class="text-3xl pt-8 pb-10">
+  <span class="text-red-500">BLOCK</span><br/>
+  <span class="text-green-500">PAPER</span><br/>
+  <span class="text-blue-500">SCISSORS</span>
+</h1>
+
+<Divider size="medium" />
+
+<div class="mb-10"></div>
 
 <fund-ring-widget
   contractAddress="0xA369Ae5472c0F6E78BaBb73Baa624a5F694DEe51"
